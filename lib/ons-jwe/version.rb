eed8c524a61eeb34177072b41ac55449e58da064
John Topley <john.topley@ons.gov.uk>
2017-03-02 11:58:05 +0000

    CTPA-1141 No longer a major version change as the method signatures are the same

--- lib/ons-jwe/version.rb
+++ lib/ons-jwe/version.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 module ONSJWE
   module Version
-    MAJOR = 2
-    MINOR = 0
+    MAJOR = 1
+    MINOR = 1
     TINY  = 0
   end
   VERSION = [Version::MAJOR, Version::MINOR, Version::TINY].compact * '.'
